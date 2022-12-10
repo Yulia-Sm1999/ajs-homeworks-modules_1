fff2904b79c5a98b6488da3c1a0168c51f178eaf
Юлия Смирнова <dolphin_99@mail.ru>
2022-12-10 21:39:35 +0300

    Changed package.json and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,19 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   plugins: [
     new HTMLWebpackPlugin({
       template: './src/index.html',
-      filename: 'bandle.html'
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.js$/,
@@ -24,10 +22,6 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
-    ]
+    ],
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bandle.js'
-  }
 };
